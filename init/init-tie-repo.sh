d6a6b8decafea0386bd1dc40df16caf175ca44f5
Gustav Tschirschnitz <gustavtschirschnitz@googlemail.com>
2022-01-29 18:37:25 +0000

    make gcc-11 after installation the default; add a script for checking out and build the tie repository

---
+++ init/init-tie-repo.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd $1
+git clone https://github.com/gtsc/take-it-easy.git
+cd take-it-easy
+git submodule update --init
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -GNinja
+ninja
--- init/install-compilers.sh
+++ init/install-compilers.sh
@@ -5,5 +5,12 @@ $base_path/upgrade-packages.sh
 echo "INFO: Installing all Tools required for Compiling"
 echo "================================================="
 
-sudo apt-get install gcc-11 g++-11 -y
 sudo apt-get install cmake -y
+sudo apt-get install ninja-build -y
+sudo apt-get install gcc-11 g++-11 -y
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+sudo update-alternatives --set gcc /usr/bin/gcc-11
+
+export CC=gcc
+export CXX=g++
